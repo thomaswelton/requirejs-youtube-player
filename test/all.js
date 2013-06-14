7ed2f1103b83f96c9feac76837b6324f7e937614
Thomas Welton <thomas@clicklabs.co.uk>
2013-06-14 11:06:29 +0100

    fixes #1 added test to compare source against compiled files

--- test/all.js
+++ test/all.js
@@ -1,5 +1,26 @@
+'use strict';
+
+var grunt = require('grunt');
+var path = require('path');
+var fs = require('fs');
+var coffee = require('coffee-script');
+
 exports["test some stuff"] = function(test){
     test.expect(1);
     test.ok(true, "this assertion should pass");
     test.done();
+};
+
+exports["Test compiled against source"] = function(test){
+	test.expect(1);
+
+	// Get the JS commited in the project
+	var commited = grunt.file.read('dist/YouTubePlayer.js');
+
+	var src = grunt.file.read('src/YouTubePlayer.coffee');
+	var compiled = coffee.compile(src);
+
+	test.equal(commited, compiled, "Compiled Javascipt up to date with CoffeeScript source");
+
+	test.done();
 };
